7fb1e7313b738d69c95ee6bf92759a1ce63a5e8f
GajaRaputa <32999912+GajaRaputa@users.noreply.github.com>
2017-10-22 15:39:42 +0200

    NetBeans project files upload

---
+++ life/src/life/Life.java
@@ -0,0 +1,13 @@
+package life;
+
+/**
+ *
+ * @author Gaja Raputa, Agnieszka Wojtowicz
+ */
+public class Life {
+
+    public static void main(String[] args) {
+        System.out.println("Life");
+    }
+    
+}
